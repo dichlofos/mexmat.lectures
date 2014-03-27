c6f368b4612ba4974acb634bb45bf637c9c25028
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-03-27 10:29:27 +0400

    Symlinking all source files (tex,sty,eps) into generated/ destination directory

--- run-latex.sh
+++ run-latex.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -xe
 latex_cmd="$1"
 shift
+
 mkdir -p generated
-( cd generated && $latex_cmd $@ )
+cd generated
+for i in ../*.{tex,sty,eps} ; do
+    ln -sf "$i" .
+done
+$latex_cmd $@
