6e743c71151036c51a5731a0f2559765ae73e82c
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-01-29 01:35:18 +0300

    Fixes

--- repl.sh
+++ repl.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
+set -xe
 filez=$(find . -name 'CMakeLists.txt')
 for c in $filez ; do
-    if ! grep -i mmtodo $c ; then
+    if ! grep -qi mmtodo $c ; then
         continue
     fi
-    d=$(dirname $c)
+    d=$(dirname $c) | cut -c 3-
     sed -i -e "s:Add $d:Add:" $c
     echo $d
     echo $c
