87ba23f9b322f2514e411fcf36a2f6f8e9c282ec
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-02-05 22:37:06 +0300

    Fix log message

--- run-mpost.sh
+++ run-mpost.sh
@@ -12,7 +12,7 @@ log_file="run_mpost.log"
 ln -sf ../$pic_name .
 if ! $mpost_cmd "$@" "$pic_name" > $log_file 2>&1 ; then
     echo "There was an error processing command:"
-    echo "    $mpost_cmd $@"
+    echo "    $mpost_cmd $@" "$pic_name"
     echo "Removing output files"
     echo "==================================== MPOST ERROR LOG ===="
     cat $log_file
